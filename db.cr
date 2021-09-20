fdaddb1e14b5b2bc155ed488f3163007f9f262bc
Kanezoh <asfaltmanta114@gmail.com>
2021-09-20 18:02:18 +0900

    add db env

--- db.cr
+++ db.cr
@@ -2,8 +2,8 @@ require "mysql"
 
 def select_all_data
   books = [] of Hash(String, Int32|String)
-  DB.open "mysql://new_app:mypassword@localhost/new_app" do |db|
-    rs = db.query "select id, name from books"
+  DB.open ENV["DB_STR"] do |db|
+    rs = db.query "select id, title from books"
     rs.each do
       books << {"id" => rs.read(Int32), "name" => rs.read(String)}
     end
