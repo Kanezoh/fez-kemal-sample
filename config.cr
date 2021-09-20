b73aad8c9b050e9b60eead2f0e75635e7450af03
Kanezoh <asfaltmanta114@gmail.com>
2021-09-20 19:38:45 +0900

    improve env

--- config.cr
+++ config.cr
@@ -4,7 +4,9 @@ require "kemal"
 require "./src/macros/*"
 require "./src/new_app.cr"
 
-File.read_lines(".env").each do |line|
-  key, value = line.strip.split "="
-  ENV[key] = value
+if File.exists?(".env")
+  File.read_lines(".env").each do |line|
+    key, value = line.strip.split "="
+    ENV[key] = value
+  end
 end
