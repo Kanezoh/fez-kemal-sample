5b3683c997e017c072f0ce7c596f21583376b727
Kanezoh <asfaltmanta114@gmail.com>
2021-09-24 12:18:33 +0900

    カラムを追加

--- create_table.sql
+++ create_table.sql
@@ -2,6 +2,8 @@ create table articles (
 id int AUTO_INCREMENT,
 title VARCHAR(255) NOT NULL,
 content MEDIUMTEXT NOT NULL,
+content_summary VARCHAR(255) NOT NULL,
+img_s3 VARCHAR(255),
 created_at DATETIME default current_timestamp NOT NULL,
 updated_at timestamp default current_timestamp on update current_timestamp NOT NULL,
 PRIMARY KEY(id)
