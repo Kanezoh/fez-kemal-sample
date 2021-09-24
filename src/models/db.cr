ff0b6ae4a9439996b213be2e6eb253288a413869
Kanezoh <asfaltmanta114@gmail.com>
2021-09-24 12:18:57 +0900

    一覧ページのデザイン

--- src/models/db.cr
+++ src/models/db.cr
@@ -4,9 +4,9 @@ class Article
   def self.fetch_all
     articles = [] of Hash(String, Int32|String)
     DB.open ENV["DB_STR"] do |db|
-      rs = db.query "select id, title from articles"
+      rs = db.query "select id, title, content_summary from articles"
       rs.each do
-        articles << {"id" => rs.read(Int32), "name" => rs.read(String)}
+        articles << {"id" => rs.read(Int32), "title" => rs.read(String), "content_summary" => rs.read(String)}
       end
     end
     articles
