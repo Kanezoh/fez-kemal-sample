c26960f2ee5e64dec82ceb7004b71e2c78862d50
Kanezoh <asfaltmanta114@gmail.com>
2021-09-26 08:11:15 +0900

    add show action

--- src/models/article.cr
+++ src/models/article.cr
@@ -17,4 +17,19 @@ class Article
     end
     articles
   end
+
+  def self.find(id)
+    DB.open ENV["DB_STR"] do |db|
+      rs = db.query "select id, title, content_summary, img_url, created_at from articles where id = ?", id
+      rs.each do
+        return {
+          "id" => rs.read(Int32),
+          "title" => rs.read(String),
+          "content_summary" => rs.read(String),
+          "img_url" => rs.read(String),
+          "created_at" => rs.read(Time).as(Time).to_local.to_s("%F")
+        }
+      end
+    end
+  end
 end
--- src/new_app.cr
+++ src/new_app.cr
@@ -5,6 +5,12 @@ get "/" do
   view("articles/index")
 end
 
-get "/articles/:id" do
-  view("articles/show")
+get "/articles/:id" do |env|
+  id = env.params.url["id"]
+  article = Article.find(id)
+  if article.nil?
+    render_404
+  else
+    view("articles/show")
+  end
 end
