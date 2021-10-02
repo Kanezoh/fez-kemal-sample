cf1b968f5d53659296f905c44fc7db410c7025d0
Kanezoh <asfaltmanta114@gmail.com>
2021-10-02 22:36:38 +0900

    setting ogp

--- src/new_app.cr
+++ src/new_app.cr
@@ -4,6 +4,7 @@ require "./models/*"
 get "/" do
   title = "哲学と技術の次元"
   articles = Article.fetch_all
+  ogp_str = ogp_tag("", title, "哲学と技術、思考を整理するためのブログ、雑記", "https://res.cloudinary.com/dhn2agtz0/image/upload/v1633181396/home_slrrxz.jpg")
   view("articles/index")
 end
 
@@ -14,10 +15,23 @@ get "/articles/:id" do |env|
     render_404
   else
     title = "哲学と技術の次元 | #{article["title"]}"
+    ogp_str = ogp_tag("articles/#{id}", title, article["content_summary"], article["img_url"])
     view("articles/show")
   end
 end
 
 get "/sitemap" do |env|
   env.redirect "https://kanezoh-blog-bucket.s3.us-east-2.amazonaws.com/sitemaps/sitemap.xml"
+end
+
+def ogp_tag(path, title, content_summary, img_url)
+  type = path == "" ? "blog" : "article"
+  <<-EOS
+  <meta property="og:url" content="/#{path}" />
+  <meta property="og:type" content="#{type}" />
+  <meta property="og:title" content="#{title}" />
+  <meta property="og:description" content="#{content_summary}" />
+  <meta property="og:site_name" content="哲学と技術の次元" />
+  <meta property="og:image" content="#{img_url}" />
+  EOS
 end
