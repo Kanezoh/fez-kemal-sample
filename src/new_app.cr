830963238c584fb701783d473cc8d86296d0ad73
Kanezoh <asfaltmanta114@gmail.com>
2021-11-14 10:28:57 +0900

    change title

--- src/new_app.cr
+++ src/new_app.cr
@@ -2,9 +2,9 @@ require "xml"
 require "./models/*"
 
 get "/" do
-  title = "哲学と技術の次元"
+  title = "かねぞうドロップ"
   articles = Article.fetch_all
-  ogp_str = ogp_tag("", title, "哲学と技術、思考を整理するためのブログ、雑記", "https://res.cloudinary.com/dhn2agtz0/image/upload/v1633181396/home_slrrxz.jpg")
+  ogp_str = ogp_tag("", title, "思考を整理するためのブログ、技術記事と雑記", "https://res.cloudinary.com/dhn2agtz0/image/upload/v1633181396/home_slrrxz.jpg")
   view("articles/index")
 end
 
@@ -14,7 +14,7 @@ get "/articles/:id" do |env|
   if article.nil?
     render_404
   else
-    title = "#{article["title"]} | 哲学と技術の次元"
+    title = "#{article["title"]} | かねぞうドロップ"
     ogp_str = ogp_tag("articles/#{id}", title, article["content_summary"], article["img_url"])
     view("articles/show")
   end
@@ -31,7 +31,7 @@ def ogp_tag(path, title, content_summary, img_url)
   <meta property="og:type" content="#{type}" />
   <meta property="og:title" content="#{title}" />
   <meta property="og:description" content="#{content_summary}" />
-  <meta property="og:site_name" content="哲学と技術の次元" />
+  <meta property="og:site_name" content="かねぞうドロップ" />
   <meta property="og:image" content="#{img_url}" />
   EOS
 end
